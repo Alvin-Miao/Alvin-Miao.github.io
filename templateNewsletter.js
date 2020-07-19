c1b6f9a627b8fa841ec3e28d19e5700bff44b682
Alvin-Miao <67649728+Alvin-Miao@users.noreply.github.com>
2020-07-19 23:22:20 +0800

    Add files via upload

---
+++ templateNewsletter.js
@@ -0,0 +1,53 @@
+// Add a header
+    // Info
+    $("body").prepend(`
+        <div class="title">
+            <div align="center"><h1>Newsletters</h1></div>
+            <hr>
+            <h2>Chief Editor:</h2>
+            <h4>Alvin Miao</h4>
+            <h2>Editors:</h2>
+            <h4>Alvin Miao, Neelofar Temboli, Vincent Hou</h4>
+            <h2>School:</h2>
+            <h4>The High School Affiliated to Renmin University of China (RDFZ)</h4>
+            <h4>Princetomn International School of Mathematics and Science (PRISMS)</h4>
+        </div>
+        <br>
+    `)
+
+// Add a footer
+    // Contect Information
+    $("body").append(`
+        <br>
+        <div class="footer" align="left">
+            <b>Alvin Miao</b>
+            <br>
+            <b>Email: </b>alvin.miao@prismsus.org
+            <br>
+            <b>Address: </b>19 Lambert Drive, Princeton, NJ, America (PRISMS)
+    `)
+
+// Add a navigation bar
+    // Navigation Bar
+    $("body").prepend(`
+        <div align="center" style=font-size:25px class="navigation_bar">
+            Navigation Bar
+            <hr>
+            <a href="Alvin_1.html">Self Introduction</a>
+            <br>
+            <br>
+            <a href="Alvin_2.html">Alvin in Daily Life</a>
+            <br>
+            <br>
+            <a href="Alvin_3.html">My Resume</a>
+            <br>
+            <br>
+            <a href="Alvin_4.html">Contact Me</a>
+            <br>
+            <br>
+            <a href="Alvin_5.html">Addtional Resources</a>
+            <br>
+            <br>
+            <a href="Alvin.html">Back to Home Page</a>
+        </div>
+    `)
